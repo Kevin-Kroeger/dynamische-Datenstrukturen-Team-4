6539c7c6ec2684096fa787d94adfa8fe8ef047fe
Felix Wienands <felix.wienands@hebk.de>
2022-11-06 21:27:51 +0100

    Init Project

--- src/main/java/de/hebk/Main.java
+++ src/main/java/de/hebk/Main.java
@@ -1,7 +1,13 @@
 package de.hebk;
 
+import de.hebk.model.list.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        //Erstellt eine List für den Typ String
+        List<String> l1 = new List<String>();
+
         System.out.println("Hello world!");
     }
 }
