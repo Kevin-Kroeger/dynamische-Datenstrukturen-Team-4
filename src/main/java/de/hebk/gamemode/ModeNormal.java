3f902e994ee7b4fb7d69353a7e2c1e789b4ec17e
Kevin-Kroeger <kevin.kroeger@hebk.de>
2022-12-02 09:21:33 +0100

    [ADD] ModeNormal.java Getter/Setter & Variables

--- src/main/java/de/hebk/gamemode/ModeNormal.java
+++ src/main/java/de/hebk/gamemode/ModeNormal.java
@@ -1,6 +1,25 @@
 package de.hebk.gamemode;
 
-import de.hebk.gamemode.Gamemode;
+import de.hebk.model.list.List;
 
 public class ModeNormal extends Gamemode {
+
+    private List<Questions> questions;
+    private int money;
+
+    public List<Questions> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Questions> questions) {
+        this.questions = questions;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
 }
