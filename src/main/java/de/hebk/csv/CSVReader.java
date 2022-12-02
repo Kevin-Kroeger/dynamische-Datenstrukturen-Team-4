0efeb2f55cfc25415ac7964caaa1b601decca7fe
Kevin-Kroeger <kevin.kroeger@hebk.de>
2022-12-02 08:57:40 +0100

    [ADD] CSVReader.readCSVList

--- src/main/java/de/hebk/csv/CSVReader.java
+++ src/main/java/de/hebk/csv/CSVReader.java
@@ -1,4 +1,39 @@
 package de.hebk.csv;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import de.hebk.gamemode.*;
+import de.hebk.model.list.*;
+
 public class CSVReader {
+
+    public List<Questions> readCSVList(String path){
+        List<Questions> l1 = new List<Questions>();
+        String line;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            while((line = br.readLine()) != null){
+                String[] arr = line.split(",");
+                String[] questions = new String[4];
+                for(int i = 0, j = 2; i < questions.length; i++,j++){
+                    questions[i] = arr[j];
+                }
+                int diff = Integer.parseInt(arr[1]);
+                l1.append(new Questions(arr[0], questions,arr[2],diff));
+            }
+            br.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return l1;
+    }
 }
