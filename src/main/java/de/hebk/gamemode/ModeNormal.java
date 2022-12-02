f5a5e3723605a9d636e3aa5d243fbe42cc98535f
Kevin-Kroeger <kevin.kroeger@hebk.de>
2022-12-02 16:34:22 +0100

    [ADD] ModeNormal nextQuestion changeLVL checkCorrect

--- src/main/java/de/hebk/gamemode/ModeNormal.java
+++ src/main/java/de/hebk/gamemode/ModeNormal.java
@@ -1,8 +1,6 @@
 package de.hebk.gamemode;
 
 import de.hebk.model.list.List;
-import de.hebk.model.node.Node;
-
 import java.util.Random;
 
 public class ModeNormal extends Gamemode {
@@ -10,17 +8,26 @@ public class ModeNormal extends Gamemode {
     private List<Questions> questions;
     private int money;
     private int lvl;
+    private Questions currentQuestion;
+    private int count = 0;
+
 
     public ModeNormal(List<Questions> pList){
         setQuestions(pList);
         setLvl(1);
+        setMoney(0);
+    }
+
+
+    public boolean checkCorrect(String pAnswer){
+        return pAnswer.equals(currentQuestion.getCorrect());
     }
 
     /**
-     * 
+     *
      * @return
      */
-    public Questions randomQuestion(){
+    public void randomQuestion(){
         Random rand = new Random();
         List<Questions> list = new List<>();
         questions.toFirst();
@@ -37,12 +44,31 @@ public class ModeNormal extends Gamemode {
                 list.next();
             }
         }
-        return list.getObject();
+        currentQuestion = list.getObject();
     }
 
+    /**
+     *
+     */
     public void nextQuestion(){
-
+        String s = "ja";
+        if(checkCorrect(s)){
+            count++;
+            changeLVL();
+            randomQuestion();
+        }
     }
+
+    public void changeLVL(){
+        if(count == 5){
+            setLvl(2);
+        }else if(count == 10){
+            setLvl(3);
+        }
+    }
+
+
+
 
     public List<Questions> getQuestions() {
         return questions;
@@ -66,5 +92,13 @@ public class ModeNormal extends Gamemode {
 
     public void setLvl(int lvl) {
         this.lvl = lvl;
+    }
+
+    public Questions getCurrentQuestion() {
+        return currentQuestion;
+    }
+
+    public void setCurrentQuestion(Questions currentQuestion) {
+        this.currentQuestion = currentQuestion;
     }
 }
